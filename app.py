fca95426a8c6e2a359f790d1db1361bb934caa40
Happy <insisthzr@163.com>
2021-12-15 21:19:52 +0800

    sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
+import click
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{os.path.join(app.root_path,"data.db")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 name = 'Happy'
 movies = [
@@ -19,4 +26,39 @@ movies = [
 
 @app.route('/')
 def index():
+    user = User.query.first()
+    movies = Movie.query.all()
     return render_template('index.html', name=name, movies=movies)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop')
+def init_db(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m.get('title'), year=m.get('year'))
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
